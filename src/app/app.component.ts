4d25fd380a619997bf744b3cb3b0fd2bd98cf60f
Kevin Cuba Castillo <cck2023657@est.univalle.edu>
2024-09-12 06:45:43 -0400

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule,MatButtonModule,MatToolbarModule,MatIconModule,MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'amblp_gestion_app';
+  showFiller = false;
+
 }
---
+++ src/app/components/clubes/clubes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clubes',
+  standalone: true,
+  imports: [],
+  templateUrl: './clubes.component.html',
+  styleUrl: './clubes.component.css'
+})
+export class ClubesComponent {
+
+}
