387a5d8bfd2396d86513418751bf521429dd30b1
KevinCubaCastillo <cck2023657@est.univalle.edu>
2024-09-24 22:12:28 -0400

    Registro de clubes mas listado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatListModule} from '@angular/material/list'
+import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule,MatButtonModule,MatToolbarModule,MatIconModule,MatMenuModule, MatListModule],
+  imports: [RouterOutlet, MatSidenavModule,MatButtonModule,MatToolbarModule,MatIconModule,MatMenuModule, MatListModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/jugadores/jugadores.component.ts
+++ src/app/components/jugadores/jugadores.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
 
 @Component({
   selector: 'app-jugadores',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './jugadores.component.html',
   styleUrl: './jugadores.component.css'
 })
